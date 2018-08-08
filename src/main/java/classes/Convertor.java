c5945f1de37d79197f91770f71979cc8c721d8b6
Ruaridh <ruaridhdunbar@gmail.com>
2018-08-08 20:00:57 +0100

    Sets lower and upper bound for possible inputs, passes tests to not allow numbers below 1 or above 3999. Refactors function to use a for loop to append I to a string and passes tests for converting numbers up to 3.

--- src/main/java/classes/Convertor.java
+++ src/main/java/classes/Convertor.java
@@ -5,6 +5,13 @@ import interfaces.RomanNumeralGenerator;
 public class Convertor implements RomanNumeralGenerator {
 
     public String generate(int number) {
-        return "I";
+        StringBuilder numeral = new StringBuilder();
+        if ((number > 0) && (number < 4000)){
+            for (int i = 0; i < number; i++){
+                numeral.append("I");
+            }
+            return numeral.toString();
+        }
+        return "Please only enter numbers between 1 - 3999";
     }
 }
--- src/test/java/ConvertorTest.java
+++ src/test/java/ConvertorTest.java
@@ -15,5 +15,18 @@ public class ConvertorTest {
     @Test
     public void canConvert(){
         assertEquals("I", convertor.generate(1));
+        assertEquals("II", convertor.generate(2));
+        assertEquals("III", convertor.generate(3));
+        assertEquals("IV", convertor.generate(4));
+    }
+
+    @Test
+    public void cantEnterNumberBelow1(){
+        assertEquals("Please only enter numbers between 1 - 3999", convertor.generate(0));
+    }
+
+    @Test
+    public void cantEnterNumberAbove3999(){
+        assertEquals("Please only enter numbers between 1 - 3999", convertor.generate(4000));
     }
 }
