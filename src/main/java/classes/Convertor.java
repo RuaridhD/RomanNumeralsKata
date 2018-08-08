3f398f8c787584d3a895379c0861b57807dc0a3f
Ruaridh <ruaridhdunbar@gmail.com>
2018-08-08 19:45:41 +0100

    passes test for converting 1 to 'I'

--- src/main/java/classes/Convertor.java
+++ src/main/java/classes/Convertor.java
@@ -5,6 +5,6 @@ import interfaces.RomanNumeralGenerator;
 public class Convertor implements RomanNumeralGenerator {
 
     public String generate(int number) {
-        return null;
+        return "I";
     }
 }
--- src/test/java/ConvertorTest.java
+++ src/test/java/ConvertorTest.java
@@ -1,2 +1,19 @@
+import classes.Convertor;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class ConvertorTest {
+
+    Convertor convertor;
+
+    @Before
+    public void before(){
+        convertor = new Convertor();
+    }
+
+    @Test
+    public void canConvert(){
+        assertEquals("I", convertor.generate(1));
+    }
 }
