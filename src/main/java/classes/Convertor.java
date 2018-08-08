fd497cb644cc22e6010fd6dccae003f2efc6300f
Ruaridh <ruaridhdunbar@gmail.com>
2018-08-08 20:17:42 +0100

    Passes tests for converting numbers up to 4

--- src/main/java/classes/Convertor.java
+++ src/main/java/classes/Convertor.java
@@ -6,11 +6,16 @@ public class Convertor implements RomanNumeralGenerator {
 
     public String generate(int number) {
         StringBuilder numeral = new StringBuilder();
-        if ((number > 0) && (number < 4000)){
-            for (int i = 0; i < number; i++){
-                numeral.append("I");
+        if ((number > 0) && (number < 4000)) {
+            if (number == 4) {
+                numeral.append("IV");
+                return numeral.toString();
+            } else {
+                for (int i = 0; i < number; i++) {
+                    numeral.append("I");
+                }
+                return numeral.toString();
             }
-            return numeral.toString();
         }
         return "Please only enter numbers between 1 - 3999";
     }
