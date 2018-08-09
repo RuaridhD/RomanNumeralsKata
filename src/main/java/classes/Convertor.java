fa95dbd87111aba862c99c3cf95340af26816bed
Ruaridh <ruaridhdunbar@gmail.com>
2018-08-09 13:47:22 +0100

    Refactored function to use a TreeMap to store the corresponding Roman Numeral values to each number, and makes the function calculating the conversion recursive so that it displays correct numerals for numbers after 20. Passes tests for converting numbers up to 20 so far.

--- src/main/java/classes/Convertor.java
+++ src/main/java/classes/Convertor.java
@@ -2,34 +2,28 @@ package classes;
 
 import interfaces.RomanNumeralGenerator;
 
+import java.util.TreeMap;
+
 public class Convertor implements RomanNumeralGenerator {
 
     public String generate(int number) {
-        StringBuilder numeral = new StringBuilder();
-        int counter = number;
-        if ((number > 0) && (number < 4000)) {
-            if (number >= 10) {
-                numeral.append("X");
-                counter -= 10;
-            }
-            if (counter == 9) {
-                numeral.append("IX");
-                counter -= 9;
-            }
-            if (counter >= 5) {
-                numeral.append("V");
-                counter -= 5;
-            }
-            if (counter == 4) {
-                numeral.append("IV");
-                counter -= 4;
-            }
-            for (int i = 0; i < counter; i++) {
-                numeral.append("I");
-            }
-            return numeral.toString();
+        TreeMap<Integer, String> numeralValues = new TreeMap();
+        numeralValues.put(1, "I");
+        numeralValues.put(4, "IV");
+        numeralValues.put(5, "V");
+        numeralValues.put(9, "IX");
+        numeralValues.put(10, "X");
 
+        StringBuilder numeral = new StringBuilder();
+
+        if ((number > 0) && (number < 4000)) {
+            int nearestNumber = numeralValues.floorKey(number);
+            if (number == nearestNumber) {
+                return numeralValues.get(number);
+            }
+            return numeralValues.get(nearestNumber) + generate(number - nearestNumber);
         }
         return "Please only enter numbers between 1 - 3999";
     }
+
 }
